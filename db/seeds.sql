9f2b7b068b070a8e0a0b6cbdd048277cb861e403
Ben Hilliard <benhilliard23@hotmail.com>
2020-08-15 12:43:42 +0930

    heroku attempt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 USE burgers_db;
 
 insert into burgers (burger_name, devoured)
-values ("big bertha", true);
+values ("big bertha burger", false);
 
 insert into burgers (burger_name)
-values ("finchy's feast");
+values ("finchy's feast burger");
 
 insert into burgers (burger_name, devoured)
-values ("spicy deluxe", true);
+values ("spicy deluxe burger", true);
 
 select * from burgers
